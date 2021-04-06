cfd909fa32450e15c0321ffd26b47c64e9b4ccb3
Nguyễn Trang <trangnguyenfcp@gmail.com>
2021-04-06 17:04:14 +0700

    06/04/2021

---
+++ module2/src/Bai_6_Ke_thua/Bai_tap/Thuc_hanh_1/ShapeTest.java
@@ -0,0 +1,10 @@
+package Bai_6_Ke_thua.Bai_tap.Thuc_hanh_1;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }}
