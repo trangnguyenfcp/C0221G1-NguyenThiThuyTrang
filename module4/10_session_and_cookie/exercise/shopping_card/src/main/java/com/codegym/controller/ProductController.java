64b3151e6f13fee9c536c2ac416fcb189a77b735
Trang Nguyễn <trangnguyenfcp@gmail.com>
2021-07-01 03:38:29 +0700

    update bai tap gio hang

--- module4/10_session_and_cookie/exercise/shopping_card/src/main/java/com/codegym/controller/ProductController.java
+++ module4/10_session_and_cookie/exercise/shopping_card/src/main/java/com/codegym/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.codegym.model.entity.Product;
 import com.codegym.model.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,5 +41,38 @@ public class ProductController {
         }
         cart.addProduct(productOptional.get());
         return "redirect:/shop";
+    }
+
+    @GetMapping("/view/{id}")
+    public String viewProduct(@PathVariable Long id, @ModelAttribute Cart cart,
+                              Model model) {
+        Product productOptional = productService.findById(id).get();
+        if (productOptional == null ) {
+            return "/error.404";
+        }
+        model.addAttribute("product",productOptional);
+        return "view";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, @ModelAttribute Cart cart){
+        Product product = productService.findById(id).get();
+        if (product == null ) {
+            return "/view.error.404";
+        }
+        cart.removeProduct(product);
+        return "redirect:/shopping-cart";
+    }
+
+    @GetMapping("/change/{id}")
+    public String changeProductQuantity(@PathVariable Long id,
+                                     @ModelAttribute Cart cart,
+                                     @RequestParam("action") String action){
+        Product product = productService.findById(id).get();
+        if (product == null ) {
+            return "/error.404";
+        }
+        cart.changeProductQuantity(product,action);
+        return "redirect:/shopping-cart";
     }
 }
