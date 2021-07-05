99aeb796c6fad9b4f9cf894bc11b080b13ba10df
Trang Nguyễn <trangnguyenfcp@gmail.com>
2021-07-05 20:07:21 +0700

    05/07/2021

---
+++ module4/14_spring_security/practice/spring_security/src/main/java/com/example/security/SecurityUnit.java
@@ -0,0 +1,6 @@
+package com.example.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityUnit extends AbstractSecurityWebApplicationInitializer {
+}
